f6f74bc2e2a995d403b6d243e86275d441bdacd7
Denis Komarov <deniskomarrov@yandex.ru>
2025-07-16 12:42:35 +0200

    Day 2

---
+++ Day_2/Day2_advent.R
@@ -0,0 +1,76 @@
+# Input
+input <- scan("Day_2/input_advent.txt", what = numeric(), sep = ",")
+
+# Part A
+
+# Replace position
+input[2] <- 12
+input[3] <- 2
+
+# Process the array
+i <- 1
+while (i <= length(input)) {
+    # Read first 4 positions
+    opcode <- input[i]
+    # Fucking R starts indexing at 1, bruh
+    pos1 <- input[i + 1] + 1
+    pos2 <- input[i + 2] + 1
+    pos3 <- input[i + 3] + 1
+
+    if (opcode == 99) {
+        break
+    }
+
+    if (opcode == 1) {
+        input[pos3] <- input[pos1] + input[pos2]
+    } else if (opcode == 2) {
+        input[pos3] <- input[pos1] * input[pos2]
+    }
+
+    # Move to the next 4 numbers
+    i <- i + 4
+}
+print (input[1])
+
+# Part B
+
+found <- FALSE
+for (noun in 0:99) {
+    for (verb in 0:99) {
+        # Reset
+        input <- scan("Day_2/input_advent.txt", what = numeric(), sep = ",")
+         # Replace positions
+        input[2] <- noun
+        input[3] <- verb
+        # Process the array
+        i <- 1
+        while (i <= length(input)) {
+            # Read first 4 positions. Fucking R starts indexing at 1, bruh
+            opcode <- input[i]
+            pos1 <- input[i + 1] + 1
+            pos2 <- input[i + 2] + 1
+            pos3 <- input[i + 3] + 1
+
+            if (opcode == 99) {
+                break
+            }
+
+            if (opcode == 1) {
+                input[pos3] <- input[pos1] + input[pos2]
+            } else if (opcode == 2) {
+                input[pos3] <- input[pos1] * input[pos2]
+            }
+
+            # Move to the next 4 numbers
+            i <- i + 4
+        }
+
+        if (input[1] == 19690720) {
+            print(paste(noun, verb))
+            print(paste(100 * noun + verb))
+            found <- TRUE
+            break
+        }
+    }
+    if (found) break
+}
