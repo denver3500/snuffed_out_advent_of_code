ade8ae56a754cc54b6c93ce5dcb255ff738db2b3
Denis Komarov <deniskomarrov@yandex.ru>
2025-08-10 11:46:44 +0200

    Days 12-13

---
+++ Day_12/Day12_advent.R
@@ -0,0 +1,92 @@
+library(combinat)
+library(numbers)
+
+# Input
+start_time <- Sys.time()
+lines <- readLines("Day_12/input_advent.txt")
+coords <- do.call(rbind, lapply(lines, function(line) {
+  nums <- as.integer(unlist(regmatches(line, gregexpr("-?\\d+", line))))
+  return(nums)
+}))
+colnames(coords) <- c("x", "y", "z")
+
+# Part A
+
+# Initialize velocity
+velocities <- matrix(0, nrow = nrow(coords), ncol = 3)
+colnames(velocities) <- c("x", "y", "z")
+# Unique permutations
+pairs <- combn(nrow(coords), 2)
+
+for (step in 1:1000) {
+  # Apply gravity
+  for (k in 1:ncol(pairs)) {
+    i <- pairs[1, k]
+    j <- pairs[2, k]
+    for (axis in 1:3) {
+      if (coords[i, axis] < coords[j, axis]) {
+        velocities[i, axis] <- velocities[i, axis] + 1
+        velocities[j, axis] <- velocities[j, axis] - 1
+      } else if (coords[i, axis] > coords[j, axis]) {
+        velocities[i, axis] <- velocities[i, axis] - 1
+        velocities[j, axis] <- velocities[j, axis] + 1
+      }
+    }
+  }
+
+  # Apply velocity
+  coords <- coords + velocities
+}
+
+# Calculate energies
+potential_energy <- apply(abs(coords), 1, sum)
+kinetic_energy <- apply(abs(velocities), 1, sum)
+total_energy <- potential_energy * kinetic_energy
+
+print(sum(total_energy))
+end_time1 <- Sys.time()
+print(end_time1 - start_time)
+# Part B
+
+coords <- do.call(rbind, lapply(lines, function(line) {
+  nums <- as.integer(unlist(regmatches(line, gregexpr("-?\\d+", line))))
+  return(nums)
+}))
+colnames(coords) <- c("x", "y", "z")
+
+find_zero <- function(coords, axis) {
+  initial_coords <- coords[, axis]
+  velocities <- rep(0, length(initial_coords))
+  step <- 0
+  repeat {
+    # Apply gravity
+    for (i in 1:(length(coords[, axis]) - 1)) {
+      for (j in (i + 1):length(coords[, axis])) {
+        if (coords[i, axis] < coords[j, axis]) {
+          velocities[i] <- velocities[i] + 1
+          velocities[j] <- velocities[j] - 1
+        } else if (coords[i, axis] > coords[j, axis]) {
+          velocities[i] <- velocities[i] - 1
+          velocities[j] <- velocities[j] + 1
+        }
+      }
+    }
+    # Apply velocity
+    coords[, axis] <- coords[, axis] + velocities
+    step <- step + 1
+
+    if (all(coords[, axis] == initial_coords) && all(velocities == 0)) {
+      return(step)
+    }
+  }
+}
+
+x_cycle <- find_zero(coords, 1)
+y_cycle <- find_zero(coords, 2)
+z_cycle <- find_zero(coords, 3)
+
+system_cycle <- LCM(x_cycle, LCM(y_cycle, z_cycle))
+print(system_cycle)
+
+end_time2 <- Sys.time()
+print(end_time2 - start_time)
---
+++ Day_13/Day13_advent.R
@@ -0,0 +1,53 @@
+source("Day_11/intcode.R")
+# Input
+input <- scan("Day_13/input_advent.txt", what = numeric(), sep = ",")
+
+# Part A
+run <- intcode(input)
+outputs <- run$outputs
+
+tiles <- data.frame(
+    x  = outputs[seq(1, length(outputs), 3)],
+    y  = outputs[seq(2, length(outputs), 3)],
+    id = outputs[seq(3, length(outputs), 3)]
+)
+
+# Count block tiles (id == 2)
+block_count <- sum(tiles$id == 2)
+print(block_count)
+
+# Part B
+
+input <- scan("Day_13/input_advent.txt", what = numeric(), sep = ",")
+input[1] <- 2
+
+state <- NULL
+inputs <- c()
+score <- 0
+paddle_x <- NA
+ball_x <- NA
+
+repeat {
+    res <- intcode(input, inputs = inputs, state = state)
+    outputs <- res$outputs
+    
+    for (k in seq(1, length(outputs), by = 3)) {
+        x <- outputs[k]
+        y <- outputs[k + 1]
+        v <- outputs[k + 2]
+
+        if (x == -1 && y == 0) {
+            score <- v
+        } else {
+            if (v == 3) paddle_x <- x
+            if (v == 4) ball_x <- x
+        }
+    }
+
+    if (isTRUE(res$halted)) break
+    move <- if (is.na(ball_x) || is.na(paddle_x)) 0 else sign(ball_x - paddle_x)
+    inputs <- c(move)
+    state <- res$state
+}
+
+print(score)
